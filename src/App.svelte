0204ff77906c51e5518246244db79777e484f539
LAPTOP-CNMITHM8\user <you@example.com>
2022-05-11 20:49:26 +0600

    ready a basic svelete app for project spaceX

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import Home from './pages/Home.svelte';
+	import CompanyInfo from './pages/CompanyInfo.svelte';
+	import Launch from './pages/Launch.svelte';
+	import LaunchPads from './pages/LaunchPads.svelte';
+	import NotFound from './pages/NotFound.svelte';
+
+let routes = {
+	"/": Home,
+	"/company": CompanyInfo,
+	"/launch": Launch,
+	"/launch-pads": LaunchPads
+
+}
+
+</script>
+
+<!-- <nav>
+	<a href="#/">Home</a>
+	<a href="#/company">Company Information</a>
+	<a href="#/launch">Launch</a>
+	<a href="#/launch-pads">Launch Pads</a>
+</nav> -->
+
+<main>
+	<Router {routes}/>
+</main>
+
+<style>
+	
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/**/*.{html,js,svelte}',
+    './public/**/*.{html,js,svelte}',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
